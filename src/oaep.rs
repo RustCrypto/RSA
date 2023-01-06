009519b08439b6855f07ba23bcdc24a68c394440
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2023-01-06 08:19:06 +0200

    feat: relax Sized requirement for random source parameters
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

--- src/oaep.rs
+++ src/oaep.rs
@@ -23,7 +23,7 @@ const MAX_LABEL_LEN: u64 = 2_305_843_009_213_693_951;
 ///
 /// [PKCS#1 OAEP]: https://datatracker.ietf.org/doc/html/rfc8017#section-7.1
 #[inline]
-pub fn encrypt<R: CryptoRngCore, K: PublicKey>(
+pub fn encrypt<R: CryptoRngCore + ?Sized, K: PublicKey>(
     rng: &mut R,
     pub_key: &K,
     msg: &[u8],
@@ -80,7 +80,7 @@ pub fn encrypt<R: CryptoRngCore, K: PublicKey>(
 ///
 /// [PKCS#1 OAEP]: https://datatracker.ietf.org/doc/html/rfc8017#section-7.1
 #[inline]
-pub fn decrypt<R: CryptoRngCore, SK: PrivateKey>(
+pub fn decrypt<R: CryptoRngCore + ?Sized, SK: PrivateKey>(
     rng: Option<&mut R>,
     priv_key: &SK,
     ciphertext: &[u8],
@@ -104,7 +104,7 @@ pub fn decrypt<R: CryptoRngCore, SK: PrivateKey>(
 /// `rng` is given. It returns one or zero in valid that indicates whether the
 /// plaintext was correctly structured.
 #[inline]
-fn decrypt_inner<R: CryptoRngCore, SK: PrivateKey>(
+fn decrypt_inner<R: CryptoRngCore + ?Sized, SK: PrivateKey>(
     rng: Option<&mut R>,
     priv_key: &SK,
     ciphertext: &[u8],
