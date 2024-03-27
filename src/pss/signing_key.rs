c925b4991de3006e00935000901e392a276c98a4
LWEdslev <202010638@post.au.dk>
2024-03-27 21:48:02 +0100

    use verify_algorithm_id for pss::SigningKey decoding

--- src/pss/signing_key.rs
+++ src/pss/signing_key.rs
@@ -1,5 +1,5 @@
 use super::{get_pss_signature_algo_id, sign_digest, Signature, VerifyingKey};
-use crate::encoding::ID_RSASSA_PSS;
+use crate::encoding::verify_algorithm_id;
 use crate::{Result, RsaPrivateKey};
 use const_oid::AssociatedOid;
 use core::marker::PhantomData;
@@ -232,9 +232,7 @@ where
     type Error = pkcs8::Error;
 
     fn try_from(private_key_info: pkcs8::PrivateKeyInfo<'_>) -> pkcs8::Result<Self> {
-        private_key_info
-            .algorithm
-            .assert_algorithm_oid(ID_RSASSA_PSS)?;
+        verify_algorithm_id(&private_key_info.algorithm)?;
         RsaPrivateKey::try_from(private_key_info).map(Self::new)
     }
 }
