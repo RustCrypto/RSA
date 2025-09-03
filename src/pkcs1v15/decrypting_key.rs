93a29ae5ef88e2630fbe60ea21c5c6fab2cad1f9
Tony Arcieri <bascule@gmail.com>
2025-09-02 22:08:19 -0600

    Revert "Minimum modulus size checks (#526)" (#575)
    This reverts commit 77d999692c8a3c6653e8dca9c6f5a811bef1bba5.
    
    Seeing test failures after this was merged:
    
    https://github.com/RustCrypto/RSA/actions/runs/17422515248/job/49463477362

--- src/pkcs1v15/decrypting_key.rs
+++ src/pkcs1v15/decrypting_key.rs
@@ -64,7 +64,7 @@ mod tests {
 
         let mut rng = ChaCha8Rng::from_seed([42; 32]);
         let decrypting_key =
-            DecryptingKey::new(RsaPrivateKey::new(&mut rng, 1024).expect("failed to generate key"));
+            DecryptingKey::new(RsaPrivateKey::new(&mut rng, 64).expect("failed to generate key"));
 
         let tokens = [
             Token::Struct {
@@ -73,27 +73,9 @@ mod tests {
             },
             Token::Str("inner"),
             Token::Str(concat!(
-                "30820278020100300d06092a864886f70d0101010500048202623082025e0",
-                "2010002818100cd1419dc3771354bee0955a90489cce0c98aee6577851358",
-                "afe386a68bc95287862a1157d5aba8847e8e57b6f2f94748ab7efda3f3c74",
-                "a6702329397ffe0b1d4f76e1b025d87d583e48b3cfce99d6a507d94eb46c5",
-                "242b3addb54d346ecf43eb0d7343bcb258a31d5fa51f47b9e0d7280623901",
-                "d1d29af1a986fec92ba5fe2430203010001028181009bb3203326d0c7b31f",
-                "456d08c6ce4c8379e10640792ecad271afe002406d184096a707c5d50ee00",
-                "1c00818266970c3233439551f0e2d879a8f7b90bd3d62fdffa3e661f14c8d",
-                "cce071f081966e25bb351289810c2f8a012f2fa3f001029d7f2e0cf24f6a4",
-                "b139292f8078fac24e7fc8185bab4f02f539267bd09b615e4e19fe1024100",
-                "e90ad93c4b19bb40807391b5a9404ce5ea359e7b0556ee25cb2e7455aeb5c",
-                "af83fc26f34457cdbb173347962c66b6fe0c4686b54dbe0d2c913a7aa924e",
-                "ff6031024100e148067566a1fa3aabd0672361be62715516c9d62790b03f4",
-                "326cc00b2f782e6b64a167689e5c9aebe6a4cf594f3083380fe2a0a7edf1f",
-                "325e58c523b981a0b3024100ab96e85323bd038a3fca588c58ddd681278d6",
-                "96e8d84ef7ef676f303afcb7d728287e897a55e84e8c8b9e772da447b3115",
-                "8d0912877fa7d4945b4d15c382f7d102400ddde317e2e36185af01baf7809",
-                "2b97884664cb233e9421002d0268a7c79a3c313c167b4903466bfacd4da3b",
-                "db99420df988ab89cdd96a102da2852ff7c134e5024100bafb0dac0fda53f",
-                "9c755c23483343922727b88a5256a6fb47242e1c99b8f8a2c914f39f7af30",
-                "1219245786a6bb15336231d6a9b57ee7e0b3dd75129f93f54ecf"
+                "3056020100300d06092a864886f70d010101050004423040020100020900ab",
+                "240c3361d02e370203010001020811e54a15259d22f9020500ceff5cf30205",
+                "00d3a7aaad020500ccaddf17020500cb529d3d020500bb526d6f"
             )),
             Token::StructEnd,
         ];
